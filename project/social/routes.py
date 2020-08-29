07217736eb45ea38e29fa825949358c6b4f03c4d
surety <zeus_polanco@hotmail.com>
2020-08-28 23:50:40 -0500

    routes and templates for portfolio and feed

--- project/social/routes.py
+++ project/social/routes.py
@@ -3,28 +3,57 @@ from flask import(
 )
 from werkzeug.exceptions import abort
 
-from app.auth import login_required
-from app import db
+from project import db
 
-bp = Blueprint('blog', __name__)
+social = Blueprint('social', __name__)
+
+def get_user(id):
+        user = db.execute(
+        'SELECT first, last, email, bio'
+        '   FROM user u'
+        '   WHERE u.id = ?',
+        (id,)
+    ).fetchone()
+
+    if user is None:
+        abort(404, "User doesn't exist.")
+
+    return user
 
 def get_student(id):
         student = db.execute(
-        'SELECT s.id, first, last, school'
+        'SELECT school, major'
         '   FROM student s JOIN user u ON s.id = u.id'
         '   WHERE u.id = ?',
         (id,)
     ).fetchone()
 
-    if student is None:
-        abort(404, "Student doesn't exist."
+    return student
 
-    return post
+def get_representative(id):
+        representative = db.execute(
+        'SELECT company'
+        '   FROM representative r JOIN user u ON r.id = u.id'
+        '   WHERE u.id = ?',
+        (id,)
+    ).fetchone()
 
-@bp.route('/int:id/portfolio', methods=('GET'))
+    return representative
+
+@social.route('/int:id/portfolio', methods=('GET'))
 def portfolio(id):
+    user = get_user(id)
     student = get_student(id)
+    representative = get_representative(id)
 
+    return render_template('social/portfolio.html', user=user, student=student, representative=representative)
 
+@social.route('/', methods=('GET'))
+def feed():
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        '   FROM post p JOIN user u ON p.author_id = u.id'
+        '   ORDER BY created DESC'
+    ).fetchall()
 
-    return render_template('social/portfolio', name=name)
+    return render_template('social/feed.html', posts=posts)
